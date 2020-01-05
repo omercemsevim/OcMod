1b03dced7009d861e5b104eac8cf208a7c0d3ebf
omercemsevim <omercemsevim@gmail.com>
2020-01-04 19:10:22 -0500

    Made an Arrow, Image is wack but the projectile works fine

---
+++ Items/NiceArrow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace OcMod.Items
+{
+    class NiceArrow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nice Arrow");
+        }
+
+        //arrow properties.
+        public override void SetDefaults()
+        {
+            item.damage = 25;
+            item.ranged = true;
+            item.width = 8;
+            item.height = 12;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.value = Item.buyPrice(copper: 10);
+            item.rare = 5;
+
+            //this creates the projectile, you give it the name of the projectile file.
+            item.shoot = mod.ProjectileType("NiceProjectile");
+            //speed of the projectile.
+            item.shootSpeed = 10f;
+            //make the item be ammo as an arrow.
+            item.ammo = AmmoID.Arrow;
+        }
+
+        //if you want to add receipe do here.
+    }
+}
---
+++ Projectiles/NiceProjectile.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+
+namespace OcMod.Projectiles
+{
+    public class NiceProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Suku Arrow");
+        }
+
+        //set properties of this arrow.
+        //in terraria you create a projectile than append its properties to an item. 
+        //For example this projectile will be used as an arrow so an arrow is created in the items folder.
+        public override void SetDefaults()
+        {
+            //width and height of the projectile hitbox
+            projectile.width = 8;
+            projectile.height = 12;
+
+            //determine the AI style of the projectile, look up what number corresponds to what online.
+            //1 - Arrow
+            projectile.aiStyle = 1;
+
+            //make sure the projectile does not hit friendly targets.
+            projectile.friendly = true;
+
+            //how many enemies does the projectile hit.
+            projectile.penetrate = 5;
+
+            //damage type.
+            projectile.ranged = true;
+
+            //set the projectile to be an arrow.
+            projectile.arrow = true;
+        }
+
+        //function to randomize if an object is created when the projectile hits the floor.
+        //you can get some arrows back.
+        public override void Kill(int timeLeft)
+        {
+            if(projectile.owner == Main.myPlayer)
+            {
+                int item = Main.rand.NextBool(5) ? Item.NewItem(projectile.getRect(), mod.ItemType("Nice Arrow")) : 0;
+            }
+        }
+
+    }
+}
